fce983d00d35bf751f8b37e7ed36e52cc31c6259
Bruno Vieira de Moura <bruno.moura.code@gmail.com>
2024-06-26 17:43:16 -0300

    Adicionado register component

---
+++ src/components/Register.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { register } from "../services/api";
+
+const Register = () => {
+  //state para formulario
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  // function para manupular dados do form
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //funcao para lidar com envio de form
+  const handleSubmit = async (e) => {
+    e.preventDeault();
+    try {
+      const newUser = await register(formData);
+      console.log("Usuario Registrado", newUser);
+    } catch (err) {
+      console.error("Erro ao registrar", err);
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="username"
+        placeholder="Nome de usuário"
+        value={formData.username}
+        onChange={handleChange}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Senha"
+        value={formData.password}
+        onChange={handleChange}
+      />
+      <button type="submit">Registrar</button>
+    </form>
+  );
+};
+
+export default Register;
